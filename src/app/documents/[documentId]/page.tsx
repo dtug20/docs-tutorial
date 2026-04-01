ec433779b0c87cbc1a42bf70eaafaae0658be3dc
Do Manh Tung <TungDM.B22CN764@stu.ptit.edu.vn>
2026-04-02 00:12:12 +0700

    update navbar and toolbar

--- src/app/documents/[documentId]/page.tsx
+++ src/app/documents/[documentId]/page.tsx
@@ -1,5 +1,6 @@
 import { ToolBar } from "./toolbar";
 import { Editor } from "./editor";
+import { Navbar } from "./navbar"
 
 interface DocumentIdPagePros {
     params: Promise<{ documentId: string }>;
@@ -9,8 +10,13 @@ const DocumentIdPage = async ({ params }: DocumentIdPagePros) => {
     const { documentId } = await params;
     return (
         <div className="min-h-screen bg-[#FAFBFD]">
-            <ToolBar />
-            <Editor />
+            <div className="flex flex-col px-4 pt-2 gap-y-2 fixed top-0 left-0 right-0 z-10 bg-[#FAFBFD] print:hidden">
+                <Navbar />
+                <ToolBar />
+            </div>
+            <div className="pt-[114px] print:pt-0">
+                <Editor />
+            </div>
         </div>
     );
 };
---
+++ src/extensions/font-size.ts
@@ -0,0 +1,54 @@
+import { Extension } from "@tiptap/core";
+import "@tiptap/extension-text-style";
+
+declare module "@tiptap/core" {
+    interface Commands<ReturnType> {
+        fontsize: {
+            setFontSize: (size: string) => ReturnType
+            unsetFontSize: () => ReturnType
+        }
+    }
+}
+
+export const FontSizeExtension = Extension.create({
+    name: "fontsize",
+    addOptions() {
+        return {
+            types: ["textStyle"],
+        }
+    },
+    addGlobalAttributes() {
+        return [
+            {
+                types: this.options.types,
+                attributes: {
+                    fontsize: {
+                        default: null,
+                        parseHTML: element => element.style.fontSize ? element.style.fontSize : null,
+                        renderHTML: attributes => {
+                            if (!attributes.fontsize) {
+                                return {};
+                            }
+                            return { style: `font-size: ${attributes.fontsize}` };
+                        }
+                    }
+                }
+            }
+        ]
+    },
+    addCommands() {
+        return {
+            setFontSize: (fontSize: string) => ({ chain }) => {
+                return chain()
+                    .setMark("textStyle", { fontsize: fontSize })
+                    .run();
+            },
+            unsetFontSize: () => ({ chain }) => {
+                return chain()
+                    .setMark("textStyle", { fontsize: null })
+                    .removeEmptyTextStyle()
+                    .run();
+            }
+        }
+    },
+})
